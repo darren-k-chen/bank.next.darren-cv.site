0fbcd5f3f594947de73a8436b764e11723c18765
Darren K.J. Chen <kjchen@pm.me>
2020-06-03 07:34:03 +0800

    Add Telegram Bot

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,25 +1,17 @@
 // Author: Darren K.J. Chen
 
-const to_addr = "kjchen@protonmail.ch";
-const init = '';
-
 function submitHandler() {
-	var name = name_text.value;
-	var subject = subject_text.value;
-	var email_addr = email_addr.value;
-	var msg = subject_text.value;
+	var name = document.getElementById("name_text").value;
+	var subject = document.getElementById("subject_text").value;
+	var email_addr = document.getElementById("email_addr").value;
+	var msg = document.getElementById("msg_text").value;
 
-	var body  = "Msg. From my personal website ( darren-k-chen.github.io )";
+	var body  = "Msg. From My Personal Website ( darren-k-chen.github.io )";
 	    body += "%0A | Client Name: " + name;
 		body += "%0A | Subject: " + subject;
 		body += "%0A | Client E-mail Addr. >> " + email_addr;
-		body += "%0A [ Client Msg ] %0A" + msg;
+		body += "%0A%0A [ Client Msg ]: " + msg;
 
-	mailTo.href = "mailto:" + email_addr + "?subject=" + subject + "&body=" + body;
-	mailTo.click();
-}
-
-function init() {
-	subject_text.value = init;
-	msg_text.value = init;
+	fetch("https://api.telegram.org/bot1174024169:AAHoakDpV9YgPtiPmvod8PQiwB-uVJyjL7I/sendmessage?chat_id=992353127&parse_mode=HTML&text=" + body);
+	alert("[Msg. Sent]: %0A" + body);
 }
